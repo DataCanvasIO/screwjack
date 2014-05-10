c83f3df9292913c37ca01308234dabdbed75e7e0
Xiaolin Zhang <leoncamel@gmail.com>
2014-05-10 06:01:09 +0000

    Fix pypi issues

--- setup.py
+++ setup.py
@@ -5,10 +5,12 @@ import os
 setup(
     name="screwjack",
     version='0.0.1',
+    description="ScrewJack is a tiny command line tool for manipulating modules.",
     author="Xiaolin Zhang",
     author_email="leoncamel@gmail.com",
-    license='MIT',
-    install_requires=['click', 'jinja2', 'requests'],
+    url="https://github.com/DataCanvasIO/screwjack",
+    license='BSD',
+    install_requires=['click>=0.6', 'jinja2>=2.7.2', 'requests>=2.2.1'],
     packages=['screwjack'],
     include_package_data = True,
     package_data = {
@@ -16,5 +18,13 @@ setup(
     },
     scripts=[
         'bin/screwjack'
+    ],
+    classifiers = [
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 2.7",
+        "Topic :: Utilities"
     ]
 )
